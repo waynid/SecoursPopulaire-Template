c053c0ceec22d03877dbcecdfc56603df9c6c6c6
Waynid <159060343+waynid@users.noreply.github.com>
2024-02-09 16:41:35 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,14 +2,17 @@
 
 $filePath = 'contacts.csv';
 
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
 $message = isset($_POST['message']) ? $_POST['message'] : '';
 $message = str_replace('"', '\"', $message);
 
-file_put_contents($filePath, implode(',', [
-    $_POST['name'] ?? '',
-    $_POST['email'] ?? '',
-    '"' . $message . '"' 
-]) . PHP_EOL, FILE_APPEND);
+$line = implode(',', [
+    '"' . $name . '"',
+    '"' . $email . '"',
+    '"' . $message . '"'
+]) . PHP_EOL;
 
+file_put_contents($filePath, $line, FILE_APPEND);
 header("Location: valid.html");
 ?>
